0eeefc211947e5ae291318428aa775ae0721c9cf
root <root@Manmeet.localdomain>
2025-06-06 14:41:17 +0530

    formatiing

--- environment/production/backend.tf
+++ environment/production/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-state-bucket"
-    key = "production/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "my-tf-state-bucket"
+    key            = "production/terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-state-lock"
-    encrypt = true
+    encrypt        = true
   }
 }
